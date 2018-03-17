1a247765a99ca1d944f67acd9c4cef2e94d96202
cholakov <cholakov@users.noreply.github.com>
2018-03-17 12:37:06 +0000

    Q2(a) & Q3(b) exploratory results

---
+++ Q2/shell_script.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+source activate nlu
+
+pwd /Users/vesko/GitHub/UoE-nlu/
+rm log.output.txt
+touch log.output.txt
+chmod +x log.output.txt
+
+for hidd_units in 5 25 50 75 100;
+do
+for lookback in 0 2 5 10;
+do
+for learn_rate in 0.5 0.7 1 1.5 2;
+do
+python /Users/vesko/GitHub/UoE-nlu/code/rnn.py train-lm /Users/vesko/GitHub/UoE-nlu/data $hidd_units $lookback $learn_rate | tee -a log.output.txt
+done
+done
+done
